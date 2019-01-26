902f1a6058bd6eed252dc1bdc89504e4d43f6bbb
rmrf51 <donat0009@outlook.com>
2019-01-27 00:15:03 +0300

    create blog, add: register, login and trash

---
+++ flaskblog.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0a3baf08265c30470bfc8c4a0ac3ed14'
+
+posts = [
+    {
+        'author': 'rmrf51',
+        'title': 'Blog Post1',
+        'content': 'First post content',
+        'date_posted': 'January 26, 2019'
+    },
+    {
+        'author': 'Nukit',
+        'title': 'Blog Post2',
+        'content': 'Second post content',
+        'date_posted': 'January 27, 2019'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
