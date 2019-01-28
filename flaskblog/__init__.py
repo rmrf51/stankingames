777275275ef00b38dfd23c22f0d211adc5c9f3c0
rmrf51 <donat0009@outlook.com>
2019-01-29 00:06:33 +0300

    account + default pic ^_^

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -7,5 +9,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '0a3baf08265c30470bfc8c4a0ac3ed14'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
